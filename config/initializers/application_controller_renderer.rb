925312de717199503b7a142b71ef3b3883817f5e
kurigaki <kenji.kurigaki@gmail.com>
2022-01-18 15:58:01 +0900

    ユーザー編集機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'e5cca368dbe5232509af49d254f1d71e63a4a86fa92e3dac3ea8edb18b00b8d324ac20c033ff9f6bf3bf198d9596f0c4c7e462f15a2613ad6ed9848196eb5ee4'
